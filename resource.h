9f5bcd346466da83e7b9702860c88fd44f13b3aa
lg144 <42222880+lg144@users.noreply.github.com>
2021-03-31 12:09:35 -0400

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PA4 Draft CAP4710.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PA4DRAFTCAP4710_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PA4DRAFTCAP4710             107
+#define IDI_SMALL                       108
+#define IDC_PA4DRAFTCAP4710             109
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     131
+#define ID_FILE_NEWGAME                 32771
+#define ID_SETTINGS_NUMBEROFAPPLES      32772
+#define ID_NUMBEROFAPPLES_5             32773
+#define ID_NUMBEROFAPPLES_10            32774
+#define ID_NUMBEROFAPPLES_20            32775
+#define ID_NUMBEROFAPPLES_CUSTOM        32776
+#define ID_SETTINGS_ADDANOBSTACLE       32777
+#define ID_APPEARANCE_SNAKECOLOR        32778
+#define ID_SNAKECOLOR_SOLID             32779
+#define ID_SNAKECOLOR_COLORFUL          32780
+#define ID_APPEARANCE_APPLESIZE         32781
+#define ID_APPLESIZE_SMALL              32782
+#define ID_APPLESIZE_MEDIUM             32783
+#define ID_APPLESIZE_LARGE              32784
+#define ID_APPEARANCE_SNAKESIZE         32785
+#define ID_SNAKESIZE_SMALL              32786
+#define ID_SNAKESIZE_MEDIUM             32787
+#define ID_SNAKESIZE_LARGE              32788
+#define ID_SETTINGS_SNAKESPEED          32789
+#define ID_SNAKESPEED_SLOW              32790
+#define ID_SNAKESPEED_MEDIUM            32791
+#define ID_SNAKESPEED_FAST              32792
+#define ID_SNAKECOLOR_CHECKERED         32793
+#define ID_SNAKECOLOR_VERTICALSTRIPPED  32794
+#define ID_SNAKECOLOR_HORIZONTALSTRIPPED 32795
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32796
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
